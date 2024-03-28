77ce843afaafbc7e8a8a8dda7f4f62abfb0a66a6
andyzheng679 <andyzheng679@gmail.com>
2024-03-28 09:18:00 -0400

    fixed file name and class name

--- src/test/java/com/zipcodewilmington/froilansfarm/Vehicle/CropDuster.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Vehicle/CropDuster.java
@@ -3,7 +3,7 @@ package com.zipcodewilmington.froilansfarm.Vehicle;
 import org.junit.Test;
 import sun.jvm.hotspot.utilities.Assert;
 
-public class VehicleCropDuster {
+public class CropDuster {
 
     //CropDuster Inheritance test
     @Test
